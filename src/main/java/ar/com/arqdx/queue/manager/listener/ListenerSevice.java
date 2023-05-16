571e166f3a90c745eae395ec987ee6407ee8423e
Cis <sabrina.cis@accenture.com>
2023-05-16 15:47:42 -0300

    listener anotation

--- src/main/java/ar/com/arqdx/queue/manager/listener/ListenerSevice.java
+++ src/main/java/ar/com/arqdx/queue/manager/listener/ListenerSevice.java
@@ -1,9 +1,11 @@
 package ar.com.arqdx.queue.manager.listener;
 
 import ar.com.arqdx.queue.manager.bean.IQueueIBMMQ;
+import ar.com.arqdx.queue.manager.properties.BrokerLoader;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jms.annotation.JmsListener;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.jms.Message;
+import java.lang.reflect.Method;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -35,16 +39,22 @@ public class ListenerSevice {
         return queue1;
     }
 
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @PostConstruct
     public void afteConstruct() {
-        log.info("<< ListenerSevice 2 >> ************* initialized MQ Listener successfully, will read from = {}",queue0.getQueueName());
-
+        log.info("<< ListenerSevice 2 >> ************* initialized MQ Listener successfully, will read from = {}", queue0.getQueueName());
+        BrokerLoader bean1 = (BrokerLoader) applicationContext.getBean("brokerLoader");
+        Map<String, IQueueIBMMQ> queues = bean1.getQueues();
+        reflection(bean1);
     }
 
     @JmsListener(destination = "#{@queue0.getQueueName()}", containerFactory = "")
- //   @JmsListener(containerFactory = "broker0.queue0.jmsListenerContainerFactory", destination="DEV.QUEUE.VALUES")
+    //   @JmsListener(containerFactory = "broker0.queue0.jmsListenerContainerFactory", destination="DEV.QUEUE.VALUES")
     public void process(Message msg) {
-        log.info("<< ListenerSevice >> @DxAnnotationJmsListener -->> MENSAJE RECIBIDO --> {}",msg);
+        log.info("<< ListenerSevice >> @DxAnnotationJmsListener -->> MENSAJE RECIBIDO --> {}", msg);
 
     }
 
@@ -52,7 +62,34 @@ public class ListenerSevice {
     //   @JmsListener(containerFactory = "broker0.queue0.jmsListenerContainerFactory", destination="DEV.QUEUE.VALUES")
     public void process2(Message msg) {
 
-        log.info("<< ListenerSevice 2 >> @DxAnnotationJmsListener -->> MENSAJE RECIBIDO --> {}",msg);
+        log.info("<< ListenerSevice 2 >> @DxAnnotationJmsListener -->> MENSAJE RECIBIDO --> {}", msg);
 
+    }
+
+
+    private void reflection(BrokerLoader bean1) {
+        ListenerSevice objetoDeMiClase = new ListenerSevice();
+        ListenerSevice b1 = applicationContext.getBean(objetoDeMiClase.getClass());
+        Class<? extends ListenerSevice> objetoDeClassConInfoDeMiClase = objetoDeMiClase.getClass();
+
+
+        // recorremos todos los métodos de cada clase
+        for (Method method : objetoDeClassConInfoDeMiClase.getDeclaredMethods()) {
+
+            // si hace uso de nuestra anotación "@MessageFilter()"
+            if (method.isAnnotationPresent(JmsListener.class)) {
+
+                String valueAtributeDestinationOfAnnotation = method.getAnnotation(JmsListener.class).destination();
+
+                String containerFactory = method.getAnnotation(JmsListener.class).containerFactory();
+
+                // y si ademas, el mensaje recibido del socket, es igual al de la anotación
+                if (containerFactory.equals("")) {
+                    System.out.println("<< PostConstruct Value Atribute Destination of Annotation :: " + valueAtributeDestinationOfAnnotation + " >>");
+                    method.getAnnotation(JmsListener.class).containerFactory().replace("","1");
+                    String containerFactory2 = method.getAnnotation(JmsListener.class).containerFactory();
+                }
+            }
+        }
     }
 }
